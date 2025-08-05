ea97e5ad0b355fe1b90f629d959b80d881acfe50
Tom <21462358@qq.com>
2025-08-05 17:23:34 +0800

    update CMakeList.txt

---
+++ exe/CMakeFiles/aidatluTool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aidatluTool.dir/aidatluTool.cxx.o"
+  "CMakeFiles/aidatluTool.dir/aidatluTool.cxx.o.d"
+  "aidatluTool"
+  "aidatluTool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aidatluTool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
